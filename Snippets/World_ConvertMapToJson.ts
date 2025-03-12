11b4473ad6caa64d35aadf5dce9929139b03fcaa
Matt Torres <matt.torres@vrinreview.com>
2025-03-12 17:53:48 -0500

    Refactor ConvertMapToJson component to enhance map initialization and JSON conversion process

--- Snippets/World_ConvertMapToJson.ts
+++ Snippets/World_ConvertMapToJson.ts
@@ -1,17 +1,22 @@
 import * as hz from 'horizon/core';
 
 class ConvertMapToJson extends hz.Component<typeof ConvertMapToJson> {
-    private map = new Map<string, string>();
+  private map = new Map<string, string>();
 
-    start() {
-        // Initialize the map with some values
-        this.map.set("id", (1001).toString());
-        this.map.set("name", "Test");
+  start() {
+    // Initialize the map with some values
+    this.map.set("id", (1001).toString());
+    this.map.set("name", "Test");
+    this.map.set("value", `${1}`); //Alternative way to convert a number to string
 
-        // Convert the map to a JSON string
-        const jsonString = JSON.stringify(this.map);
-        console.log(jsonString);
-    }
+    // Convert the map to a JSON string
+    const newObject = Object.fromEntries(this.map.entries());
+    const jsonString = JSON.stringify(newObject);
+    console.log(jsonString);
+
+    //You can copy the output in the console and save it to a json file on your computer
+    //Just open notepad, paste the output and save it as a .json file
+  }
 }
 
 hz.Component.register(ConvertMapToJson);
