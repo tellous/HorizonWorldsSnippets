c30c6a14fe7f7f4f27f53f64892eb87e787400ef
Matt Torres <matt.torres@vrinreview.com>
2025-03-12 19:54:39 -0500

    Refactor ContinuousSpin component to improve rotation logic and ensure continuous spinning

--- Snippets/World_ContinuousSpin.ts
+++ Snippets/World_ContinuousSpin.ts
@@ -6,6 +6,7 @@ class ContinuousSpin extends hz.Component<typeof ContinuousSpin> {
   private amplitude: number = 45; // Amplitude of rotation in degrees/sec
 
   start() {
+    // Connect to update event to spin continuously
     this.connectLocalBroadcastEvent(
       hz.World.onUpdate,
       this.update.bind(this)
@@ -13,11 +14,18 @@ class ContinuousSpin extends hz.Component<typeof ContinuousSpin> {
   }
 
   private update(data: { deltaTime: number }) {
+    // Accumulate rotation angle (in degrees)
     this.timeElapsed += data.deltaTime * this.amplitude;
-    this.timeElapsed %= 360; // keep angle within 0-360 degrees
     
-    const rotation = hz.Quaternion.fromEuler(new hz.Vec3(0, this.timeElapsed, 0));
+    const angle = this.timeElapsed % 360; // keep angle within 0-360 degrees
+    
+    // Create rotation quaternion from the current angle
+    const rotation = hz.Quaternion.fromEuler(new hz.Vec3(0, angle, 0));
     this.entity.rotation.set(rotation);
+
+    if (this.timeElapsed >= 360) {
+      this.timeElapsed = 0; // Reset the timer for continuous rotation
+    }
   }
 }
 hz.Component.register(ContinuousSpin);
